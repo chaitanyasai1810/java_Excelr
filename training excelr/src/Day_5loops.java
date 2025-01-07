2ac62341f06c8c81ed89a84ce95f8ca78b4abefe
Sravani Adari <sravani1acc@gmail.com>
2025-01-07 10:35:55 +0530

    first

---
+++ training excelr/src/Day_5loops.java
@@ -0,0 +1,44 @@
+public class Day_5loops
+{
+    public static void main(String args[])
+    {
+        // for(int i=1;i<=4;i++)
+        // {
+        //     System.out.print(i);
+        // }
+        // for(int i=20;i<=35;i++)
+        // {
+        //     System.out.print(i+" ");
+        // }
+        // for(int i=11;i<=19;i++)
+        // {
+        //     if(i%2==0)
+        //     {
+        //         System.out.print(i+" ");
+        //     }
+        // }
+        // public String stringTimes(String str, int n) {
+        //     String r="";
+        //     for(int i=0;i<n;i++)
+        //     {
+        //       r=r+str;
+        //     }
+        //     return r;
+        //   }
+        //   public String frontTimes(String str, int n) {
+        //     String result="";
+        //     int end=3;
+        //     if(str.length()<3)
+        //     {
+        //       end=str.length();
+        //     }
+        //     String front=str.substring(0,end);
+        //     for(int i=0;i<n;i++)
+        //     {
+        //       result=front+result;
+        //     }
+        //     return result;
+        //   }
+                    
+    }
+}
