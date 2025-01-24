007cc91d44b4f2d0da86d8745e54f30c342d3863
revanthsai8910 <revanthsai8910@gmail.com>
2025-01-24 22:57:24 +0530

    first commit

---
+++ src/day_24_01_2025.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class day_24_01_2025 {
+    public static void main(String[] args) {
+        
+    }
+}
+// class Solution 
+// {
+//     public int[] twoSum(int[] nums, int target)
+//     {    
+//         int[] result=new int[2];   
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=i+1;j<nums.length;j++)
+//             { 
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     result[0]=i;
+//                     result[1]=j;
+//                     return result;
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// }
