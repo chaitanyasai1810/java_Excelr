dba0de4a09065626271a294fb2c6a61ddf6863a3
revanthsai8910 <revanthsai8910@gmail.com>
2025-01-29 09:59:51 +0530

    first commit

---
+++ src/day_28_01_2025.java
@@ -0,0 +1,16 @@
+public class day_28_01_2025 {
+    
+}
+// import java.util.HashSet;
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>(); // Create a new HashSet to store unique integers.
+//         for (int num : nums) { // Iterate through each number in the array.
+//             if (!set.add(num)) { // Try to add the number to the HashSet.
+//                 return true; // If the number already exists, set.add() returns false, meaning a duplicate is found.
+//             }
+//         }
+//         return false; // If the loop completes, no duplicates were found, so return false.
+//     }
+// }
